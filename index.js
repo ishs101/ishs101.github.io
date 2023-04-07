554698904acc4c6346b96e7452e379c354b3b847
sw8744 <sw8744@naver.com>
2023-04-07 19:57:31 +0900

    졸방대 클릭 시 신청 창 뜨게 작업

---
+++ index.js
@@ -0,0 +1,25 @@
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
+const num3 = document.getElementById("num3");
+const num4 = document.getElementById("num4");
+const num5 = document.getElementById("num5");
+
+function openzol1() {
+    window.open('num1.html', '1번 졸방대 예약', 'width=225, height=400');
+}
+
+function openzol2() {
+    window.open('num2.html', '2번 졸방대 예약', 'width=225, height=400');
+}
+
+function openzol3() {
+    window.open('num3.html', '3번 졸방대 예약', 'width=225, height=400');
+}
+
+function openzol4() {
+    window.open('num4.html', '4번 졸방대 예약', 'width=225, height=400');
+}
+
+function openzol5() {
+    window.open('num5.html', '5번 졸방대 예약', 'width=225, height=400');
+}
