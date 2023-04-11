23dbda4b2416998d74f2faa12c8edd6519f16a1a
sw8744 <sw8744@naver.com>
2023-04-11 14:04:46 +0900

    node.js하고 MySQL 연결 성공함

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const fs = require('fs');
 const data = fs.readFileSync('./database.json');
 const conf = JSON.parse(data);
-const mysql = require('mysql');
+const mysql = require('mysql2');
 var name = "";
 
 const connection = mysql.createConnection({
     host: conf.host,
-    user: conf.username,
+    user: conf.user,
     password: conf.password,
     database: conf.database
 });
 
 connection.connect(function (err) {
-    if (err) throw err;
+    if (err) {
+        throw err;
+    }
     else {
         console.log("Connected!");
     }
